683c08cee393ccb7f3324b08f17e3f7966845f4d
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-04-25 03:47:31 +0400

    new: system module added with basic routing capabilities

---
+++ modules/system/server/routes/system.js
@@ -0,0 +1,16 @@
+// System route
+
+module.exports = function(System){
+	// Add routes here manually
+	// toDo: refactor to add this to configurations / automation
+	var routes = [];
+
+	// Include routes for all modules defined
+	routes.forEach(function(route){
+		var router = require('../../../' + route.module + '/server/routes/' + route.module)(System);
+		System.app.use('/' + route.root, router);
+
+		System.log.info(route.module + ' route set');
+
+	});
+};
