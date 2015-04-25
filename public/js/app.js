98ecba25ee6bb8adb1d04ca8136fe8639f605f21
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-04-25 16:50:11 +0400

    edit: theming added cyan+yellow

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,18 @@
-// Include app dependency on ngMaterial
+// Angular App
 	angular.module('app', ['ngMaterial', 'calocial.users'])
+		.config(['$mdThemingProvider', function($mdThemingProvider) {
+			$mdThemingProvider.theme('default')
+				.primaryPalette('cyan', {
+					'default': '500',
+					'hue-1': '100',
+					'hue-2': '500',
+					'hue-3': '700'
+				})
+				.accentPalette('yellow', {
+					'default': 'A200',
+					'hue-1': 'A100',
+					'hue-2': 'A400'
+				});
+		}])
 		.controller("appController", function(){
 		});
