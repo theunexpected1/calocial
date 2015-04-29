56211d01f32fbe91237c93f97197801616080517
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-04-29 20:09:32 +0400

    new: posts route added

---
+++ modules/posts/server/routes/posts.js
@@ -0,0 +1,14 @@
+// Posts route
+
+module.exports = function(System){
+	// Modules
+	var controller = require('../controllers/posts')(System),
+		router = System.express.Router();
+
+	router
+		.post('/', controller.create)
+		.get('/', controller.get)
+		.get('/:postId', controller.get);
+
+	return router;
+};
