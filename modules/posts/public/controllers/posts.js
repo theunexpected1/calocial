6e3c8afdae920c10a4cf9d2ca02ee42cfa1b65f5
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-02 19:28:01 +0400

    update: streamlined posts related stuff into it's own controller

--- modules/posts/public/controllers/posts.js
+++ modules/posts/public/controllers/posts.js
@@ -8,10 +8,34 @@ angular.module('calocial.posts')
 		'$location',
 		function($rootScope, $scope, $resource, $location){
 			$scope.posts = {};
-			$resource('/meetings').get(function(res){
-				if(res.status){
-					$scope.posts = res.json;
-				}
-			});
+			$scope.post = {};
+			$scope.isCreatingPost = false;
+
+			$scope.createOrCancelPost = function(){
+				$scope.isCreatingPost = !$scope.isCreatingPost;
+			}
+
+			$scope.getPosts = function(){
+				$resource('/meetings').get(function(res){
+					if(res.status){
+						$scope.posts = res.json;
+					}
+				});
+			}
+
+			$scope.create = function(){
+				$resource('/meetings').save($scope.post, function(res){
+					if(res.status){
+						console.log('post saved');
+						console.log(res.json);
+					} else{
+						console.log('error in saving post: ' + res.message);
+						console.log(res.json);
+					}
+				});
+			}
+
+
+			$scope.getPosts();
 		}
 	]);
